6449e5b8e974b8f6dda78f283e514bcbd093e2b1
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-21 14:55:36 +0100

    add validate and defaults

--- server/server.js
+++ server/server.js
@@ -5,23 +5,45 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }, completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-let newTodo = new Todo({
-    text: 'Program entire website',
-    completed: true,
-    completedAt: new Date()
+// let newTodo = new Todo({
+//    text: '  Make a js game  '
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save todo', e );
+// });
+
+const User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
 });
 
-newTodo.save().then((doc) => {
-    console.log('Saved todo', doc);
-}, (e) => {
-    console.log('Unable to save todo', e );
+let newUser = new User({
+    email: 'fred@bedrock.com'
 });
+
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save todo', e);
+})
